36a4bbb4cf06eb1145781f87d00533edaa96a69f
tryproxy <tryproxy@protonmail.com>
2026-05-04 22:16:03 +0300

    refactor: app component template remove

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import type { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { LayoutComponent } from './shared/components/layout/layout.component';
+import { ROUTES } from './shared/config/routes';
 
-export const routes: Routes = [];
+const filmDetails = () =>
+  import('./features/film-details/film-details.component').then(m => m.FilmDetailsComponent);
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: ROUTES.HOME.path,
+        title: 'Library',
+        component: HomeComponent,
+      },
+      {
+        path: ROUTES.DETAILS.path,
+        title: 'Movie Details',
+        loadComponent: filmDetails,
+      },
+    ],
+  },
+];
