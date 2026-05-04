23edb85a7a2612384482128e02762f079ac873be
tryproxy <tryproxy@protonmail.com>
2026-05-04 22:17:53 +0300

    feat: init film service

---
+++ src/app/core/services/films.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FilmsService {
+  // public listMovies(): any {
+  //   return;
+  // }
+  // public retrieveMovie(id: number): any {
+  //   return;
+  // }
+}
